2223f100787020e8e87d64115ca65b79cd882b3a
Fabiola Lombardi Medina <fabiolalombardi@MacBook-Air-de-Fabiola.local>
2021-01-17 19:18:52 +0100

    search page completed

--- src/pages/daoexplorer/List.tsx
+++ src/pages/daoexplorer/List.tsx
@@ -1,5 +1,134 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  styled,
+  Typography,
+  withTheme,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { SearchInput } from "./components/SearchInput";
+import { MockDAOs } from "../../store/mock/mock";
 
-export const DAOsList = () => {
-  return <div>DAO LIST</div>;
+const GridContainer = styled(Grid)({
+  paddingRight: "6%",
+  paddingLeft: "6%",
+  paddingTop: "4%",
+  marginBottom: 60,
+  background: "inherit",
+});
+
+const StyledButton = styled(Button)({
+  height: 69,
+});
+
+const TotalDao = styled(Typography)({
+  marginRight: 37,
+  lineHeight: "124.3%",
+  letterSpacing: "-0.01em",
+});
+
+const DaoContainer = styled(withTheme(Grid))((props) => ({
+  height: 179,
+  border: "2px solid #3D3D3D",
+  boxSizing: "border-box",
+  background: props.theme.palette.primary.main,
+  boxShadow: "none",
+  borderRadius: 0,
+  paddingTop: 42,
+  borderTop: "none",
+  "&:nth-child(odd)": {
+    borderLeft: "none",
+    paddingLeft: "6%",
+  },
+  "&:nth-child(even)": {
+    borderRight: "none",
+    borderLeft: "none",
+    paddingLeft: "3%",
+    paddingRight: "6%",
+  },
+  "&:hover": {
+    background: "rgba(129, 254, 183, 0.03)",
+    borderLeft: "2px solid #81FEB7",
+    cursor: "pointer",
+  },
+}));
+
+const GridBackground = styled(Grid)({
+  background: "inherit",
+  borderTop: "2px solid #3D3D3D",
+});
+
+export const DAOsList: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
+  const [currentDAOs, setCurrentDAOs] = useState(MockDAOs);
+
+  const history = useHistory();
+
+  const filterDAOs = (filter: string) => {
+    setSearchText(filter);
+    const filtered = MockDAOs.filter((dao: any) => {
+      return dao.name.toLowerCase().includes(searchText);
+    });
+    if (filtered.length > 0 && filter !== "") {
+      setCurrentDAOs(filtered);
+    } else {
+      setCurrentDAOs(MockDAOs);
+    }
+    return;
+  };
+
+  return (
+    <Box bgcolor="primary.main" width="100%" height="100%">
+      <GridContainer container direction="row" alignItems="center">
+        <Grid item xs={6}>
+          <SearchInput search={filterDAOs} />
+        </Grid>
+        <Grid
+          container
+          item
+          direction="row"
+          justify="flex-end"
+          alignItems="center"
+          alignContent="center"
+          xs={6}
+        >
+          <TotalDao color="textSecondary" variant="subtitle1">
+            {currentDAOs.length} DAOs
+          </TotalDao>
+          <StyledButton
+            color="secondary"
+            variant="outlined"
+            onClick={() => history.push("/creator")}
+          >
+            Create DAO
+          </StyledButton>
+        </Grid>
+      </GridContainer>
+      <GridBackground container direction="row">
+        {currentDAOs.map((dao: any) => {
+          return (
+            <DaoContainer item xs={6} key={dao.symbol}>
+              <Typography variant="subtitle1" color="secondary">
+                {dao.symbol}
+              </Typography>
+              <Grid container direction="row" alignItems="center">
+                <Grid item xs={6}>
+                  <Typography variant="h4" color="textSecondary">
+                    {dao.name}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle1" color="textSecondary">
+                    {dao.voting_addresses} VOTING ADDRESSES
+                  </Typography>
+                </Grid>
+              </Grid>
+            </DaoContainer>
+          );
+        })}
+      </GridBackground>
+    </Box>
+  );
 };
---
+++ src/store/mock/mock.ts
@@ -0,0 +1,47 @@
+export const MockDAOs = [
+  {
+    name: "Tezos DAO",
+    symbol: "TEZDAO",
+    voting_addresses: 115,
+  },
+  {
+    name: "ANTIDAO",
+    symbol: "ANTIDAO",
+    voting_addresses: 12,
+  },
+  {
+    symbol: "PONY",
+    name: "Pony Collective",
+    voting_addresses: 46,
+  },
+  {
+    symbol: "VGOV",
+    name: "Vault Governance",
+    voting_addresses: 189,
+  },
+  {
+    symbol: "REODAO",
+    name: "Reo Coin DAO",
+    voting_addresses: 72,
+  },
+  {
+    symbol: "FIREDAO",
+    name: "tFire DAO",
+    voting_addresses: 21,
+  },
+  {
+    name: "NuBank Org",
+    symbol: "NUBANK",
+    voting_addresses: 131,
+  },
+  {
+    name: "Lowdown DAO",
+    symbol: "LOWDOWN",
+    voting_addresses: 230,
+  },
+  {
+    symbol: "REODAO2",
+    name: "REODAO DAO",
+    voting_addresses: 33,
+  },
+];
