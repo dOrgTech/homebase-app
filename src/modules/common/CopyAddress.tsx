b29a7144ed13dc21f9005e9722129dd973ad6a0d
fabiolalombardim <fabiolalombardi@MacBook-Air-de-Fabiola.local>
2021-03-14 18:14:29 +0100

    after copied effect added

--- src/modules/common/CopyAddress.tsx
+++ src/modules/common/CopyAddress.tsx
@@ -4,15 +4,17 @@ import { CopyButton } from "./CopyButton";
 
 export const CopyAddress: React.FC<{ address: string }> = ({ address }) => {
   return (
-    <Grid container alignItems="center">
-      <Grid item>
-        <Typography variant="subtitle1" color="textSecondary">
-          {address}
-        </Typography>
+    <>
+      <Grid container alignItems="center">
+        <Grid item>
+          <Typography variant="subtitle1" color="textSecondary">
+            {address}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <CopyButton text={address} />
+        </Grid>
       </Grid>
-      <Grid item>
-        <CopyButton text={address} />
-      </Grid>
-    </Grid>
+    </>
   );
 };
