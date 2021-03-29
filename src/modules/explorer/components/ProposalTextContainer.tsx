5ae669efa4306c473aebf547806cdbf74ae808c7
Ben <benefacto@users.noreply.github.com>
2021-03-29 11:01:23 -0700

    Add proposal title UI to proposal dialogs (#152)
    Closes #141

---
+++ src/modules/explorer/components/ProposalTextContainer.tsx
@@ -0,0 +1,70 @@
+import { styled, Grid, Typography, TextField } from "@material-ui/core";
+import { Field } from "formik";
+import React from "react";
+
+export const CustomTextarea = styled(TextField)({
+  textAlign: "end",
+  "& .MuiInputBase-multiline": {
+    textAlign: "initial",
+    border: "1px solid #434242",
+    boxSizing: "border-box",
+    "& .MuiInputBase-inputMultiline": {
+      padding: 12,
+      textAlign: "initial",
+    },
+  },
+  paddingTop: 24
+});
+
+export const DescriptionContainer = styled(Grid)({
+  paddingLeft: 24,
+  paddingRight: 24,
+  paddingTop: 24,
+});
+
+export const ProposalTextContainer: React.FC<{ title: string, value: string, type: "description" | "title" }> = ({ title, value, type }) => {
+  return (
+    <DescriptionContainer container direction="row">
+      <Grid item xs={12}>
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          justify="space-between"
+        >
+          <Grid item xs={6}>
+            <Typography
+              variant="subtitle1"
+              color="textSecondary"
+            >
+              {title}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography
+              align="right"
+              variant="subtitle1"
+              color="textSecondary"
+            >
+              {value
+                ? value.trim().split(" ")
+                  .length
+                : 0}{" "}
+            Words
+          </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Field
+          name={type}
+          type="string"
+          multiline
+          rows={type === "description" ? 6 : 2}
+          placeholder={`Type a ${title}`}
+          component={CustomTextarea}
+        />
+      </Grid>
+    </DescriptionContainer>
+  );
+};
