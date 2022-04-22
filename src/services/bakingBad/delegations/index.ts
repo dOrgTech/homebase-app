20d214caaa61704bc03e8d0cf53a72b64b65f7c0
namesty <nestor09amesty@gmail.com>
2022-04-22 06:31:57 +0200

    (feat): showing current delegate and redirection to baking bad

---
+++ src/services/bakingBad/delegations/index.ts
@@ -0,0 +1,22 @@
+import { Network } from "services/beacon/context";
+import { networkNameMap } from "..";
+import { DelegationDTO } from "./types";
+
+export const getLatestDelegation = async (
+  daoAddress: string,
+  network: Network,
+) => {
+  const url = `https://api.${networkNameMap[network]}.tzkt.io/v1/operations/delegations?sender=${daoAddress}&status=applied`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch delegations from TZKT API");
+  }
+
+  const resultingDelegations: DelegationDTO[] = await response.json();
+
+  if (!resultingDelegations.length) {
+    return null
+  }
+
+  return resultingDelegations[0]
+};
