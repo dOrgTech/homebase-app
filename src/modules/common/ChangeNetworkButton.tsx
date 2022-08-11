9975136da5b66cb97dc47960a5bd79e104345f45
Manank Patni <manank321@gmail.com>
2022-08-11 13:20:52 +0530

    Upgrade V1 to Jakartanet and Other bug fixes
    Bug fix of proposal creation fail as well as blank screen on
    clicking asset choose dropdown
    
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/modules/common/ChangeNetworkButton.tsx
+++ src/modules/common/ChangeNetworkButton.tsx
@@ -105,11 +105,11 @@ export const NetworkMenu: React.FC<
         <AddressMenuItem
           container
           alignItems="center"
-          onClick={() => handleNetworkChange("hangzhounet")}
+          onClick={() => handleNetworkChange("jakartanet")}
         >
           <Grid item>
             <Typography variant="subtitle2" color="textSecondary">
-              HANGZHOUNET
+              JAKARTANET
             </Typography>
           </Grid>
         </AddressMenuItem>
