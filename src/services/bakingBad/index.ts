6db3f0925b53960a1868eaaed65e3dc19828f4ab
Manank Patni <manank321@gmail.com>
2022-10-04 05:03:29 +0530

    Fix better call API issue

--- src/services/bakingBad/index.ts
+++ src/services/bakingBad/index.ts
@@ -1,6 +1,6 @@
 import { Network } from "../beacon/context";
 
-export const API_URL = "https://api.better-call.dev/v1";
+export const API_URL = "https://bff.better-call.dev/v1";
 export const networkNameMap: Record<Network, string> = {
   mainnet: "mainnet",
   jakartanet: "jakartanet",
