55522a04498dc6c31819105c614b1ed90701983a
Manank Patni <manank321@gmail.com>
2022-10-05 22:13:49 +0530

    Add kathmandunet support
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/services/bakingBad/index.ts
+++ src/services/bakingBad/index.ts
@@ -2,5 +2,6 @@ import { Network } from "services/beacon"
 
 export const networkNameMap: Record<Network, string> = {
   mainnet: "mainnet",
-  ghostnet: "ghostnet"
+  ghostnet: "ghostnet",
+  kathmandunet: "kathmandunet"
 }
