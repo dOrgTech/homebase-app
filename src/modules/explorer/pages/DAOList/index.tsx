3b87fc1d2141de5ef7145375cd75f4ca318850d1
Manank Patni <manank321@gmail.com>
2022-12-07 19:12:56 +0530

    Add explorer page to tezos-homebase
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/modules/explorer/pages/DAOList/index.tsx
+++ src/modules/explorer/pages/DAOList/index.tsx
@@ -152,7 +152,7 @@ export const DAOList: React.FC = () => {
                     </Grid>
                   </Grid>
                   <Grid item>
-                    <Link underline="none" href={`/creator`}>
+                    <Link underline="none" href={`https://tezos-homebase.io/creator`}>
                       <MainButton variant="contained" color="secondary">
                         Create DAO
                       </MainButton>
