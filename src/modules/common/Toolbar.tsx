71da34e7cbe8b279219d218326f3d03f843fe501
Fernando Torres <80982814+thenerdcat@users.noreply.github.com>
2023-01-08 19:51:36 -0500

    Switch to relative paths (#442)
    * relative paths
    
    * relative paths part 2

--- src/modules/common/Toolbar.tsx
+++ src/modules/common/Toolbar.tsx
@@ -207,7 +207,7 @@ export const Navbar: React.FC<{
       <StyledToolbar>
         <Header container alignItems="center" wrap="wrap" justifyContent={"space-between"}>
           <Grid item>
-            <Box onClick={() => (location.href = `${getEnv(EnvKey.REACT_APP_URL)}/explorer`)}>
+            <Box onClick={() => (location.href = `/explorer`)}>
               <ToolbarContainer container alignItems="center" wrap="nowrap">
                 <Grid item>
                   <LogoItem src={HomeButton} />
@@ -331,13 +331,7 @@ export const Navbar: React.FC<{
           </Grid>
           <BackButtonContainer container justifyContent="flex-start">
             {mode !== "creator" ? (
-              <BackButtonText
-                container
-                item
-                xs={6}
-                md={2}
-                onClick={() => (location.href = `${getEnv(EnvKey.REACT_APP_URL)}/explorer`)}
-              >
+              <BackButtonText container item xs={6} md={2} onClick={() => (location.href = `/explorer`)}>
                 <BackButtonIcon />
                 <BackButton>Back</BackButton>
               </BackButtonText>
