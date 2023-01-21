608dfd6bac103ab13a77e3f542b9e0c84e648bb2
Manank Patni <manank321@gmail.com>
2023-01-21 19:49:35 +0530

    Change Hasura ENV Variables key - Bug Fix
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/services/indexer/graphql.ts
+++ src/services/indexer/graphql.ts
@@ -5,11 +5,11 @@ const BASE_URL = getEnv(EnvKey.REACT_APP_HASURA_URL_V2)
 const HASURA_ADMIN_SECRET = getEnv(EnvKey.REACT_APP_HASURA_ADMIN_SECRET_V2)
 
 if (!BASE_URL) {
-  throw new Error(`${EnvKey.REACT_APP_HASURA_URL} env variable is missing`)
+  throw new Error(`${EnvKey.REACT_APP_HASURA_URL_V2} env variable is missing`)
 }
 
 if (!HASURA_ADMIN_SECRET) {
-  throw new Error(`${EnvKey.REACT_APP_HASURA_ADMIN_SECRET} env variable is missing`)
+  throw new Error(`${EnvKey.REACT_APP_HASURA_ADMIN_SECRET_V2} env variable is missing`)
 }
 
 export const client = new GraphQLClient(BASE_URL, {
