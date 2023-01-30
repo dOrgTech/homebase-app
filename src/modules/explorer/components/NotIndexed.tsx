4cec6a2f936fbcf414ad753d370278b62ace138d
alana <alana@dorg.tech>
2023-01-29 22:48:01 -0800

    Not indexed page fix

--- src/modules/explorer/components/NotIndexed.tsx
+++ src/modules/explorer/components/NotIndexed.tsx
@@ -1,17 +1,25 @@
 import { Box, styled, Theme, Typography } from "@material-ui/core"
 import React from "react"
+import { ContentContainer } from "./ContentContainer"
 
 const SecondaryText = styled("span")(({ theme }: { theme: Theme }) => ({
   color: theme.palette.secondary.main
 }))
 
+const Hero = styled(ContentContainer)({
+  margin: "0 auto",
+  padding: "38px 38px"
+})
+
 export const NotIndexed: React.FC<{ address: string }> = ({ address }) => {
   return (
-    <Box width="100%" padding="50px">
-      <Typography variant="subtitle1" color="textSecondary" align={"center"}>
-        Your DAO with address: <br /> <SecondaryText>{address}</SecondaryText> <br /> is currently being{" "}
-        <SecondaryText>indexed</SecondaryText> and will be available in a few minutes
-      </Typography>
+    <Box>
+      <Hero>
+        <Typography variant="subtitle1" color="textPrimary" align={"center"}>
+          Your DAO with address: <br /> <SecondaryText>{address}</SecondaryText> <br /> is currently being{" "}
+          <SecondaryText>indexed</SecondaryText> and will be available in a few minutes
+        </Typography>
+      </Hero>
     </Box>
   )
 }
