bcd27b922bf182231b2fd3bcfcd2b8f21e9a40a4
fabiolalombardim <lombardifabiola@hotmail.com>
2023-04-16 19:33:22 +0200

    fix jumping boxes in faq section

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -402,6 +402,16 @@ export const theme = createTheme({
       rounded: {
         borderRadius: "8px"
       }
+    },
+    MuiAccordionSummary: {
+      root: {
+        "minHeight": 91,
+        "& .Mui-expanded": {
+          minHeight: 91,
+          display: "flex",
+          alignItems: "center"
+        }
+      }
     }
   }
 })
