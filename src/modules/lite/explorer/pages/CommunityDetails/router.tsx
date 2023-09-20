3b91e551598d0a2614c0c81c1ab4ed420906957c
Manank Patni <manank321@gmail.com>
2023-09-20 14:53:59 +0530

    Fix network switch on lite proposal
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/modules/lite/explorer/pages/CommunityDetails/router.tsx
+++ src/modules/lite/explorer/pages/CommunityDetails/router.tsx
@@ -1,11 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Switch, Route, Redirect, useRouteMatch } from "react-router"
 import { CommunityDetails } from "./index"
 import { ProposalCreator } from "../CreateProposal"
 import { ProposalDetails } from "../ProposalDetails"
+import { useCommunity } from "../../hooks/useCommunity"
+import { useTezos } from "services/beacon/hooks/useTezos"
+import { Network } from "services/beacon"
 
 export const CommunityDetailsRouter: React.FC<{ id: any }> = ({ id }): JSX.Element => {
   const match = useRouteMatch()
+  const community = useCommunity(id)
+  const { network, changeNetwork } = useTezos()
+
+  useEffect(() => {
+    if (community && community.network.toLowerCase() !== network.toLowerCase()) {
+      changeNetwork(community.network.toLowerCase() as Network)
+    }
+  }, [community, changeNetwork, network])
 
   return (
     <Switch>
