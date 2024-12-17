9e3b32409ce605cb387f5a822a8bd2d5032c731c
Ashutosh Kumar <ashutoshpw@users.noreply.github.com>
2024-12-17 09:58:37 +0530

    Enhance proposal event notification by adding JSON content type header in webhook request

--- src/services/utils/utils.ts
+++ src/services/utils/utils.ts
@@ -260,6 +260,9 @@ export const sendProposalCreatedEvent = async (
   const url = getEnv(EnvKey.REACT_APP_PROPOSAL_WEBHOOK_URL)
   const response = await fetch(url, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
       network,
       proposer: proposerAddress,
