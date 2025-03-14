142d23afa7ec41f8f244e2b4abbb3f9124b314c1
Ashutosh Kumar <ashutoshpw@users.noreply.github.com>
2025-03-14 20:43:44 +0530

    improved e2e

--- e2e/dao-v3.spec.ts
+++ e2e/dao-v3.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test"
 test("shows dao name", async ({ page }) => {
   await page.goto("/explorer/dao/KT1PY9PXm8NMAgSEZg7bUtFjmV2Sj64bKuVV/overview")
 
-  await Promise.race([page.waitForLoadState("networkidle"), page.waitForTimeout(40000)])
+  await Promise.race([page.waitForLoadState("networkidle"), page.waitForTimeout(28000)])
 
   const title = await page.locator("//*[@id='root']/div/div[1]/div/div[1]/div/div[1]/div/div[1]/p")
   const daoDescriptionEl = await page.locator("//*[@id='root']/div/div[1]/div/div[1]/div/div[2]/p")
