4bd97892c8d4d5ad95b34ec7f97b315051447b46
Ashutosh Kumar <ashutoshpw@users.noreply.github.com>
2025-03-14 20:24:44 +0530

    attempt to fix e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,12 +68,12 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  ]
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run start",
-    url: "http://127.0.0.1:3001",
-    reuseExistingServer: !process.env.CI
-  }
+  // webServer: {
+  //   command: "npm run start",
+  //   url: "http://127.0.0.1:3001",
+  //   reuseExistingServer: !process.env.CI
+  // }
 })
