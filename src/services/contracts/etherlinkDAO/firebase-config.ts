8fe2eb0e9f77f293012a70e9a19bd18cf264fafa
Ashutosh Kumar <ashutosh@w3dev.in>
2025-09-30 16:46:55 +0000

    fixed firebase database

--- src/services/contracts/etherlinkDAO/firebase-config.ts
+++ src/services/contracts/etherlinkDAO/firebase-config.ts
@@ -1,24 +1,28 @@
 import { initializeApp } from "firebase/app"
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
 import { EnvKey, getEnv } from "services/config"
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBqvj7uTsnxX8B-PNRaNI_bWoUevyx-TII",
-//   authDomain: "homebase-6907d.firebaseapp.com",
-//   projectId: "homebase-6907d",
-//   storageBucket: "homebase-6907d.firebasestorage.app",
-//   messagingSenderId: "377420069232",
-//   appId: "1:377420069232:web:4d2b785464d3a0e09077b5",
-//   measurementId: "G-67HZK5DL97"
-// }
+
+// Default Firebase Database
 const firebaseConfig = {
-  apiKey: "AIzaSyCacHn9QK3ILsPjYZ-W0BhDmwq0E5P2H7M",
-  authDomain: "tezosbase-dev.firebaseapp.com",
-  projectId: "tezosbase-dev",
-  storageBucket: "tezosbase-dev.firebasestorage.app",
-  messagingSenderId: "1092114198027",
-  appId: "1:1092114198027:web:4acf64746e224d11c1610e",
-  measurementId: "G-LRC23T5C2T"
+  apiKey: "AIzaSyBqvj7uTsnxX8B-PNRaNI_bWoUevyx-TII",
+  authDomain: "homebase-6907d.firebaseapp.com",
+  projectId: "homebase-6907d",
+  storageBucket: "homebase-6907d.firebasestorage.app",
+  messagingSenderId: "377420069232",
+  appId: "1:377420069232:web:4d2b785464d3a0e09077b5",
+  measurementId: "G-67HZK5DL97"
 }
+
+// // Ash Development Database
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCacHn9QK3ILsPjYZ-W0BhDmwq0E5P2H7M",
+//   authDomain: "tezosbase-dev.firebaseapp.com",
+//   projectId: "tezosbase-dev",
+//   storageBucket: "tezosbase-dev.firebasestorage.app",
+//   messagingSenderId: "1092114198027",
+//   appId: "1:1092114198027:web:4acf64746e224d11c1610e",
+//   measurementId: "G-LRC23T5C2T"
+// }
 const app = initializeApp(firebaseConfig)
 
 // Create Firestore instance, then (optionally) connect to emulator before exporting
