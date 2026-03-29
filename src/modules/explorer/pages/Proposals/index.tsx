58fe63acdb529a924524d1d7b8d38f544b135627
eightrice <astmobronsita@gmail.com>
2026-03-29 15:14:01 +0300

    fix: thread daoId through ProposalsList to ProposalTableRow
    The previous navigation fix didn't work because daoId was never passed
    from the full DAO proposals page through ProposalsList to
    ProposalTableRow, so route state.daoId was always undefined.

--- src/modules/explorer/pages/Proposals/index.tsx
+++ src/modules/explorer/pages/Proposals/index.tsx
@@ -307,6 +307,7 @@ const TezosProposals = () => {
                   proposals={undefined}
                   liteProposals={polls}
                   filters={filters}
+                  daoId={daoId}
                 />
               )}
               {!(polls && polls.length > 0) ? (
